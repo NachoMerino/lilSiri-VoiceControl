2401875a6d8349fd5d5b59a985953301d1059db0
josearmandojacq <castilloquiala@campus.tu-berlin.de>
2018-02-26 09:25:25 +0100

    changed to async/await

--- app/views/assets/js/modules/API/getCurrentExchangeAPI.js
+++ app/views/assets/js/modules/API/getCurrentExchangeAPI.js
@@ -1,10 +1,10 @@
 import speech from '../voiceSynthesizer.js';
 let ExchangeAPI = 'ecKDtixgL775jEe0fKZRAyu32SgG6RAI';
 
-let getCurrentExchange = (originCurrency, desiredCurrency, amount) => {
-    fetch(`https://forex.1forge.com/1.0.3/convert?from=${originCurrency}&to=${desiredCurrency}&quantity=${amount}&api_key=${ExchangeAPI}`)
-        .then(res => res.json())
-        .then(data => speech(data.text));
+async function getCurrentExchange(originCurrency, desiredCurrency, amount) {
+    let res = await fetch(`https://forex.1forge.com/1.0.3/convert?from=${originCurrency}&to=${desiredCurrency}&quantity=${amount}&api_key=${ExchangeAPI}`);
+    let data = await res.json();
+    speech(data.text)
 }
 
 export default getCurrentExchange;
--- app/views/assets/js/modules/API/weatherAPI.js
+++ app/views/assets/js/modules/API/weatherAPI.js
@@ -1,8 +1,9 @@
 import speech from '../voiceSynthesizer.js';
 
-let API = '3577128d3a684df83b143feb00e5c7e3';
+let APIKey = '3577128d3a684df83b143feb00e5c7e3';
+
 let fetchWeather = (city) => {
-  fetch(`https://api.openweathermap.org/data/2.5//weather?q=${city}&APPID=${API}`)
+  fetch(`https://api.openweathermap.org/data/2.5//weather?q=${city}&APPID=${APIKey}`)
     .then(res => res.json())
     .then(data => {
       let temperature = Math.floor(data.main.temp - 273.15);
